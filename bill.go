627780f049e66f00ad72ff6c8cdd09dfc4928d2e
n-winspear <n.winspear1@gmail.com>
2022-10-20 15:33:11 +0100

    L3 Complete

--- bill.go
+++ bill.go
@@ -12,7 +12,7 @@ func newBill(name string) bill {
 
 	b := bill{
 		name:  name,
-		items: map[string]float64{"pie": 5.99, "cake": 3.99},
+		items: map[string]float64{},
 		tip:   0,
 	}
 
@@ -26,11 +26,25 @@ func (b bill) formatBill() string {
 
 	for k, v := range b.items {
 		fs += fmt.Sprintf("%-15v ...£%v \n", k+":", v)
-
 		total += v
 	}
 
-	fs += fmt.Sprintf("%-15v ...£%0.2f", "total: ", total)
+	total += b.tip
+	fs += fmt.Sprintf("%-15v ...£%0.2f\n", "tip: ", b.tip)
+
+	fs += fmt.Sprintf("%-15v ...£%0.2f\n", "total: ", total)
 
 	return fs
+}
+
+func (b *bill) updateTip(tip float64) {
+	b.tip = tip
+}
+
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
+}
+
+func (b *bill) save() {
+
 }
