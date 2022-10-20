a37991a262b5dbe017251207745a6c52bebeda72
n-winspear <n.winspear1@gmail.com>
2022-10-20 14:50:08 +0100

    L1 content

--- main.go
+++ main.go
@@ -4,26 +4,8 @@ import "fmt"
 
 func main() {
 
-	// Arrays
+	myBill := newBill("Nathan's Bill")
 
-	var ages = [3]int{15, 24, 65}
-
-	names := [4]string{"Nathan", "Addy", "Elliot", "Angela"}
-
-	names[1] = "Adrienne"
-
-	fmt.Println(ages, len(ages))
-
-	fmt.Println(names, len(names))
-
-	// Slices
-
-	var scores = []int{100, 50, 60}
-
-	scores[2] = 25
-
-	scores = append(scores, 85)
-
-	fmt.Println(scores, len(scores))
+	fmt.Println(myBill)
 
 }
